abc02cdd8f53ac7a9366198686f80e6758763031
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-05-05 22:22:15 +0200

    Create file for writing if not exist

--- Server/src/main/java/files/writer/impl/TxtFileWriter.java
+++ Server/src/main/java/files/writer/impl/TxtFileWriter.java
@@ -2,14 +2,24 @@ package files.writer.impl;
 
 import files.writer.FileWriter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class TxtFileWriter implements FileWriter {
-    public void writeText(String path, List<String> text) throws FileNotFoundException {
-        try (var out = new PrintWriter(path)) {
+    public void writeText(String filePath, List<String> text) throws IOException {
+        Path path = Paths.get(filePath);
+
+        if (Files.notExists(path, LinkOption.NOFOLLOW_LINKS)) {
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
+        }
+
+        try (var out = new PrintWriter(path.toFile())) {
             for (String word : text) {
                 out.print(word);
             }
